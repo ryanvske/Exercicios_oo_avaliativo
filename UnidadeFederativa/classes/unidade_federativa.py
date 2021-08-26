0c500c5f097544357bf365f9af34508628a16664
Ryan Noviski <ryan.noviski@alunos.sc.senac.br>
2021-08-26 10:37:16 -0300

    Add classe UnidadeFederativa

---
+++ UnidadeFederativa/classes/unidade_federativa.py
@@ -0,0 +1,11 @@
+class Unidade_federativa:
+
+    def __init__(self):
+        self.lista_uf = ''
+        self.uf = ''
+
+    def get_lista_uf(self):
+        return self.lista_uf
+
+    def get_uf(self):
+        return self.uf
