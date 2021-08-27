601b463a3c75d78846f4a52b5f4e2e0d9f132178
Ryan Noviski <ryan.noviski@alunos.sc.senac.br>
2021-08-27 11:13:27 -0300

    add .gitignore, add ufs

--- UnidadeFederativa/classes/unidade_federativa.py
+++ UnidadeFederativa/classes/unidade_federativa.py
@@ -1,8 +1,14 @@
 class Unidade_federativa:
 
     def __init__(self):
-        self.lista_uf = ''
+        self.lista_uf = []
         self.uf = ''
+
+        self.lista_uf = ['SantaCatarina-SC',
+                         'SãoPaulo-SP',
+                         'Sergipe-SE',
+                         'Tocantins-TO']
+
 
     def get_lista_uf(self):
         return self.lista_uf
