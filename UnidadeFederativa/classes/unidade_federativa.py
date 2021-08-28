b7e1ffc42e8dd5a1200f83fa341be4c0fbedc6d5
Aluno Senac <Aluno@CDELAB2D11.alunos.sc.senac.br>
2021-08-27 21:42:14 -0300

    Add Ufs a um vetor

--- UnidadeFederativa/classes/unidade_federativa.py
+++ UnidadeFederativa/classes/unidade_federativa.py
@@ -1,6 +1,5 @@
 class Unidade_federativa:
     @staticmethod
     def get_lista_uf():
-        ufs = 'Santa_Catarina-SC', 'Sao_Paulo-SP', 'Rio_de_Janeiro-RJ'
+        ufs =[Santa_Catarina-SC, Sao_Paulo-SP, Rio_de_Janeiro-RJ]
         print(ufs)
-
--- UnidadeFederativa/main.py
+++ UnidadeFederativa/main.py
@@ -1,6 +1,5 @@
 from classes.unidade_federativa import Unidade_federativa
 
-def listar():
-    Unidade_federativa.get_lista_uf()
 
-listar()
+
+
