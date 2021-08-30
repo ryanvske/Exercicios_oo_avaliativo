d194715fdfa6f9e664f75f58e28813efa9ca065b
Aluno Senac <Aluno@CDELAB2D11.alunos.sc.senac.br>
2021-08-30 20:18:03 -0300

    add finção get_uf

--- UnidadeFederativa/classes/unidade_federativa.py
+++ UnidadeFederativa/classes/unidade_federativa.py
@@ -3,4 +3,16 @@ class Unidade_federativa:
     def get_lista_uf():
         ufs =['Santa_Catarina-SC', 'Sao_Paulo-SP', 'Rio_de_Janeiro-RJ']
         print(ufs)
+    @staticmethod
+    def get_uf():
+        ufs =['Santa_Catarina-SC', 'Sao_Paulo-SP', 'Rio_de_Janeiro-RJ']
+        uf=input('Qual UF devo retornar?')
 
+        if (uf=='SC'):
+            print(ufs[0])
+        elif (uf=='SP'):
+            print(ufs[1])
+        elif (uf=='RJ'):
+            print(ufs[2])
+        else:
+            print('UF não reconhecida.')
--- UnidadeFederativa/main.py
+++ UnidadeFederativa/main.py
@@ -2,3 +2,6 @@ from classes.unidade_federativa import Unidade_federativa
 
 print('Lista de Ufs:')
 Unidade_federativa.get_lista_uf()
+print('-------------------------')
+Unidade_federativa.get_uf()
+print('-------------------------')
